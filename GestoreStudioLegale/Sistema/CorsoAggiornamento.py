a5c2db51c515b8f041acb26af960d974584abe10
Edoardo Cecchini <S1101827@studenti.univpm.it>
2022-08-02 18:26:23 +0200

    metodo getDatiCorso

--- GestoreStudioLegale/Sistema/CorsoAggiornamento.py
+++ GestoreStudioLegale/Sistema/CorsoAggiornamento.py
@@ -12,6 +12,8 @@ class CorsoAggiornamento:
        self.dataOraFine = datetime.datetime(year=1970, month=1, day=1, hour=00, minute=00)
        self.tipo = []
 
+
+    #non dovrebbe avere un ID?
     def aggiungiCorso(self):
         if os.path.isfile('Dati\CorsiAggiornamento.pickle'):
             with open('Dati\CorsiAggiornamento.pickle', 'rb') as f:
@@ -19,3 +21,14 @@ class CorsoAggiornamento:
             corsiAggiornamento[ID] = self
         with open('Dati\CorsiAggiornamento.pickle', 'wb') as f:
             pickle.dump(corsiAggiornamento, f, pickle.HIGHEST_PROTOCOL)
+
+    def getDatiCorso(self):
+        return {
+            'Crediti': self.crediti,
+            'ID': self.ID,
+            'Data e Ora Inizio': self.dataOraInizio,
+            'Data e Ora Fine': self.dataOraFine,
+            'Tipo Corso': self.tipo,
+        }
+
+    def
