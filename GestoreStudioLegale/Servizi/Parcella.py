65430c7bb2e4347952fb536ac0f1b9e44af7539f
Laura Ferretti <s1098036@studenti.univpm.it>
2022-08-10 10:53:24 +0200

    creazione metodi "Aggiorna"

--- GestoreStudioLegale/Servizi/Parcella.py
+++ GestoreStudioLegale/Servizi/Parcella.py
@@ -11,7 +11,21 @@ class Parcella():
        self.importo = 0
        self.intestatario = ''
 
-   # def aggiornaParcella(self):
+    def aggiornaParcella(self, Cliente = None, identificativo = 0, importo = 0, intestatario = ''):
+            if Cliente != None:
+                self.Cliente = Cliente
+            elif identificativo != 0:
+                self.identificativo = identificativo
+            elif importo != 0:
+                self.importo = importo
+            elif intestatario != '':
+                self.intestatario = intestatario
+
+            self.rimuoviParcella(self.ID)
+            self.creaParcella( self.Cliente, self.ID, self.importo,
+                             self.intestatario, self.identificativo)
+            print("Aggiornato")
+
 
 
     def creaParcella(self, Cliente, ID, importo, intestatario ):
