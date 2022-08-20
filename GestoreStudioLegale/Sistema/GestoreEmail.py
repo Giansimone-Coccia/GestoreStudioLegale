19d5198a1c6c308ae5ec4a108c15f438869300c7
Edoardo Cecchini <S1101827@studenti.univpm.it>
2022-08-20 14:54:08 +0200

    modifiche GestoreEmail

--- GestoreStudioLegale/Sistema/GestoreEmail.py
+++ GestoreStudioLegale/Sistema/GestoreEmail.py
@@ -23,42 +23,41 @@ class GestoreEmail:
     def getDatiAppuntamento(self):
         return self.Appuntamento.getDatiAppuntamento()
 
-
     #il cliente come attributo?
     def invioEmail(self, emailStudio, cliente):
 
         dataInvio = self.getDatiAppuntamento().get('Data e Ora Inizio', None) - timedelta(days=1) #riguarda il delta
 
         now = datetime.now()
-    '''
-        #togliere il login
-        
-        while now != dataInvio:
-            now = datetime.now()
-            time.sleep(300)
+        '''
+            #togliere il login
+
+            while now != dataInvio:
+                now = datetime.now()
+                time.sleep(300)
 
 
-        if now == dataInvio:
-            #inizio la connesione con il server della mia email (ancora da definire)
-            smtp = smtplib.SMTP(emailStudio, 587)
-            smtp.ehlo()
-            smtp.starttls()
+            if now == dataInvio:
+                #inizio la connesione con il server della mia email (ancora da definire)
+                smtp = smtplib.SMTP(emailStudio, 587)
+                smtp.ehlo()
+                smtp.starttls()
 
-            #faccio il login
-            smtp.login('YourMail@gmail.com', 'Your Password')
-    '''
-            msg = MIMEMultipart()
+                #faccio il login
+                smtp.login('YourMail@gmail.com', 'Your Password')
+        '''
 
-            #oggetto dell'email
-            msg['Subject'] = subject
+        msg = MIMEMultipart()
 
-            #contenuto testuale
-            msg.attach(MIMEText('Le ricordiamo che il suo appuntamento è in data: ' + self.getDatiAppuntamento().get('Data e Ora Inizio', None)))
+        #oggetto dell'email
+        msg['Subject'] = subject
 
-            #utilizzatore?
-            to = cliente.getInfoUtilizzatore().get('email', None)
+        #contenuto testuale
+        msg.attach(MIMEText('Le ricordiamo che il suo appuntamento è in data: ' + self.getDatiAppuntamento().get('Data e Ora Inizio', None)))
 
-            smtp.sendmail(from_addr="hello@gmail.com",
-                      to_addrs=to, msg=msg.as_string())
+        #utilizzatore?
+        to = cliente.getInfoUtilizzatore().get('email', None)
 
-            smtp.quit()
+        smtp.sendmail(from_addr="hello@gmail.com",to_addrs=to, msg=msg.as_string())
+
+        smtp.quit()
