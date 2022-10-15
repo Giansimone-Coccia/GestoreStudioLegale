66515894957d1d5cb5aac3370dbda5dba5d29b38
Giansimone Coccia <S1097845@studenti.univpm.it>
2022-10-15 12:54:29 +0200

    Test

--- GestoreStudioLegale/Test/TestAvvocato.py
+++ GestoreStudioLegale/Test/TestAvvocato.py
@@ -5,11 +5,13 @@ from GestoreStudioLegale.Servizi.Avvocato import Avvocato
 class TestAvvocato(TestCase):
     def setUp(self):
         self.avvocatoVuoto = Avvocato()
-        self.avvocatoVuoto.creaAvvocato('', '', '', [], '', '', '', 0, '', [], [], [], [])
+        self.avvocatoVuoto.creaAvvocato('', '', '', [], '', '', '', 0, '', [], [])
         self.avvocatoOk = Avvocato()
-        self.avvocatoOk.creaAvvocato('CF', '', 'Angelo', ['Economia politica','Rischi e pericoli'], '', '', '', 0, '', [], [], '', [])
+        self.avvocatoOk.creaAvvocato('CF', 'Verdi', 'Angelo', ['Economia politica','Rischi e pericoli'], '', 'av@gmail.com', '', 0, '', [], [])
 
     def testOggettoVuoto(self):
         self.assertEqual(self.avvocatoVuoto.getDatiAvvocato()['Nome'], '')
+        self.assertEqual(self.avvocatoVuoto.getDatiAvvocato()['Codice fiscale'], '')
         self.assertEqual(len(self.avvocatoVuoto.getDatiAvvocato()['Corso aggiornamento']), 0)
         self.assertEqual(len(self.avvocatoOk.getDatiAvvocato()['Corso aggiornamento']), 2)
+        self.assertEqual(self.avvocatoOk.getDatiAvvocato()['Cognome'], 'Verdi')
--- GestoreStudioLegale/Test/TestCliente.py
+++ GestoreStudioLegale/Test/TestCliente.py
@@ -1,15 +1,21 @@
 from unittest import TestCase
 
 from GestoreStudioLegale.Servizi.Cliente import Cliente
+from GestoreStudioLegale.Sistema.CorsoAggiornamento import CorsoAggiornamento
+
 
 class TestCliente(TestCase):
     def setUp(self):
+        corso1 = CorsoAggiornamento()
+        corso2 = CorsoAggiornamento()
         self.clienteVuoto = Cliente()
-        self.clienteVuoto.creaCliente('', '', [], '', '', '', 0, '', [], [], '', [])
+        self.clienteVuoto.creaCliente('', '', [], '', '', '', 0, '', [], [], '')
         self.clienteOk = Cliente()
-        self.clienteOk.creaCliente('CF', 'Alberto', [], '', '', '', 0, '', [], [], '', ['udienza1', 'udienza2'])
+        self.clienteOk.creaCliente('CF', 'Rossi', [corso1, corso2], '', 'mr@gmail.com', '', 3334445556, '', [], [], 'Mario')
 
     def testOggettoVuoto(self):
         self.assertEqual(self.clienteVuoto.getDatiCliente()['Nome'], '')
-        self.assertEqual(len(self.clienteVuoto.getDatiCliente()['Udienza']), 0)
-        self.assertEqual(len(self.clienteOk.getDatiCliente()['Udienza']), 2)
+        self.assertEqual(len(self.clienteVuoto.getDatiCliente()['Corso aggiornamento']), 0)
+        self.assertEqual(len(self.clienteOk.getDatiCliente()['Corso aggiornamento']), 2)
+        self.assertEqual(self.clienteOk.getDatiCliente()['Codice fiscale'], 'CF')
+        self.assertEqual(self.clienteOk.getDatiCliente()['Email'], 'mr@gmail.com')
